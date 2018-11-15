2f253119dc0a4afa2461b9ee534e0d42fa5fcbde
ReasonCool <reasongm@gmail.com>
2018-11-15 16:51:43 +0800

    modify some object name

--- WebViewShareFile/WebShareView.m
+++ WebViewShareFile/WebShareView.m
@@ -11,7 +11,7 @@
     UIView *pdfView;
     CreateUIFactory*UIFactory;
     NSData*pdfData;
-    NSString*pdfDataLink;
+    NSURL*pdfDataLink;
     UIViewController* theViewController;
     
 }
@@ -31,9 +31,6 @@
 
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
-    
-    NSString *requestString = navigationAction.request.URL.absoluteString;
-    UIApplication *app = [UIApplication sharedApplication];
     NSURL *url = [navigationAction.request URL];
     pdfDataLink=url;
     if (navigationAction.targetFrame == nil) {
@@ -41,21 +38,21 @@
     }
      decisionHandler(WKNavigationActionPolicyAllow);
     
-    
 }
--(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
-    
-}
+
 - (void)webView:(WKWebView *)webView  decidePolicyForNavigationResponse:(nonnull WKNavigationResponse *)navigationResponse decisionHandler:(nonnull void (^)(WKNavigationResponsePolicy))decisionHandler{
     pdfData=nil;
-    NSString*memeTypeStr=  navigationResponse.response.MIMEType;
-    if ([memeTypeStr isEqualToString:@"application/pdf"]) {
+    NSString*mimeTypeStr=  navigationResponse.response.MIMEType;
+    if ([mimeTypeStr isEqualToString:@"application/pdf"]) {
         pdfData=[NSData dataWithContentsOfURL:pdfDataLink];
         if (pdfData) {
             [self addPDFView:pdfData URLValue:pdfDataLink];
         }
     }
      decisionHandler(WKNavigationResponsePolicyAllow);
+}
+-(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
+    
 }
 -(void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
     
@@ -80,14 +77,11 @@
 
 
 -(void)addPDFView:(NSData*)data URLValue:(NSURL*)urlValue{
-    
-   
     //bottomView add self.vbview
     CGFloat bottomHeight=64*kcscaleWidth;
     CGFloat  maxWidth=self.frame.size.width, posY=self.frame.size.height-bottomHeight,posX=16*kcscaleWidth, btnGap=20*kcscaleWidth, btnWidth=(maxWidth-posX-posX-btnGap)/2, btnHeight=32*kcscaleWidth;
     
     posY=posY+16*kcscaleWidth;
-    
     UIButton*btn=[UIFactory createBtn:CGRectMake((maxWidth-btnWidth), posY, btnWidth, btnHeight) TitleStr:@"分   享"  MainView:self ];
     [btn.layer setBorderColor:[UIColor redColor].CGColor];
     [btn.layer setBorderWidth:2];
@@ -97,15 +91,10 @@
 
     // create url
     NSURL *url = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingString:@"WebShareFile.pdf"]];
-   
-    // write data
-    
-    [pdfData writeToURL:url atomically:NO];
-    
-    
+   // write data
+   [pdfData writeToURL:url atomically:NO];
     UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[pdfDataLink, url] applicationActivities:nil];
     [theViewController presentViewController:activityVC animated:YES completion:nil];
-    
 }
 /*
 // Only override drawRect: if you perform custom drawing.
